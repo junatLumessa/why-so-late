bfdec38c001af04af4028ac600e10a2dbf09e5bd
tihig <vnissila@cs.helsinki.fi>
2017-10-22 17:55:45 +0300

    Some trial and error with OVR regression

--- code/analyze.py
+++ code/analyze.py
@@ -7,47 +7,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.dummy import DummyClassifier
 from sklearn.metrics import accuracy_score
 from sklearn import linear_model
+from sklearn.multiclass import OneVsRestClassifier
 
-
-def get_departures():
-    data = pd.read_csv('../data/data/a-train-timetablerows.csv')
-    data = data[data['type'] == 'DEPARTURE']
-    temp = []
-    percents = []
-    dates = []
-    current = data.iloc[0]['scheduledTime'][:10]
-
-    i = 0
-    j = 0
-    # sum = 0
-    for index, row in data.iterrows():
-
-        date = row['scheduledTime'][:10]
-
-        if (date > current):
-            all_len = len(temp)
-            late = [1 for i in temp if i >= 3]
-            late_len = len(late)
-            percents.append((late_len / all_len) * 100)
-            dates.append(current)
-            current = row['scheduledTime'][:10]
-
-            # print(temp)
-            # print(j)
-            temp = []
-            j = 0
-
-
-        else:
-            temp.append(row['differenceInMinutes'])
-            j += 1
-
-    mean_data = pd.DataFrame({'date': dates, 'percents': percents})
-    print(mean_data.head(n=5))
-
-    mean_data.to_csv('a-train-percents.csv', index=False)
-
-    return mean_data
 
 
 def date_shaper(df):
@@ -56,7 +17,7 @@ def date_shaper(df):
 
 
 def some_regression_thing(column):
-    training_data, test_data, train_target, test_target = divide_data(column)
+    training_data, test_data, train_target, test_target = prepare_data(column)
 
     dummy = linear_model.LinearRegression()
     dummy.fit(training_data, train_target)
@@ -75,7 +36,18 @@ def make_binarys(df, threshold, name, bool):
     else:
         df[new_name] = df[name] > threshold
 
-def divide_data(column):
+#More classes than just True/False TODO
+def make_more_binarys(df, threshold, name, bool):
+    new_name = name + '_binary'
+    if(bool):
+        for val in threshold:
+            df[new_name] = df.loc[df[name] < val, name] = val
+    else:
+        for val in threshold:
+            df[new_name] = df.loc[df[name] > val, name] = val
+
+#Get's data, makes binary variables and divides dataset into training and test set
+def prepare_data(column, multiple=False):
     # A - train data: percentages of trains late that day
     td = pd.read_csv('a-train-percents.csv')
 
@@ -83,11 +55,22 @@ def divide_data(column):
     wd = pd.read_csv('weather.csv')
     date_shaper(wd)
 
-    make_binarys(td, 5, 'percents', False)
-    if(column is 'tday'):
-        make_binarys(wd, 0, column, True)
+    if(multiple):
+        thresholds_trains = [20, 10, 5]
+        make_more_binarys(td, thresholds_trains, 'percents', False)
+
+        if (column is 'tday'):
+            thresholds_temp = [0,-5,-10]
+            make_more_binarys(wd, thresholds_temp, column, True)
+        else:
+            make_more_binarys(wd, thresholds_trains, column, False)
     else:
-        make_binarys(wd, 0, column, False)
+        make_binarys(td, 5, 'percents', False)
+
+        if(column is 'tday'):
+            make_binarys(wd, 0, column, True)
+        else:
+            make_binarys(wd, 0, column, False)
 
     df = pd.merge(td, wd, how='inner', sort=True,
                   suffixes=('_t', '_w'), copy=True, indicator=False)
@@ -101,35 +84,39 @@ def divide_data(column):
     training_data, test_data, train_target, test_target = train_test_split(explain, perc, train_size=0.8)
     #print('training_data size = ', len(training_data))
     # print('test_data size = ', range(len(test_data[0])))
-
-    train_target = train_target.values.reshape(-1, 1)
-    training_data = training_data.values.reshape(-1, 1)
-    test_target = test_target.values.reshape(-1, 1)
-    test_data = test_data.values.reshape(-1, 1)
+    if(not multiple):
+        train_target = train_target.values.reshape(-1, 1)
+        training_data = training_data.values.reshape(-1, 1)
+        test_target = test_target.values.reshape(-1, 1)
+        test_data = test_data.values.reshape(-1, 1)
 
     return training_data, test_data, train_target, test_target
 
-
-#Make data binary first, then predict it
+#Logistic regression with two binary variables (True/False)
 def logistic_regression(column):
-    training_data, test_data, train_target, test_target = divide_data(column)
+    training_data, test_data, train_target, test_target = prepare_data(column)
     logistic = LogisticRegression()
 
     logistic.fit(training_data, train_target)
     predicted_log = logistic.predict(test_data)
+    score = logistic.score(test_data, test_target)
     print(' Logistic regression score:')
-    print(logistic.score(test_data, test_target))
+    print(score)
 
-    for i in range(0, len(test_target)):
-        if (test_target[i] != predicted_log[i]):
-            print('Predicted value %d ' % predicted_log[i])
-            print('Expected value %d ' % test_target[i])
+    wrong = [1 for i in test_target if i != predicted_log[test_target.tolist().index(i)]]
+    print('Amount of wrong classified: %d ' % len(wrong))
+
+#Nice try, prediction accuracy too good.... Maybe use floats rounded as ints for prediction? TODO
+def oneVSRest(column):
+    training_data, test_data, train_target, test_target = prepare_data(column, True)
+    OVR = OneVsRestClassifier(LogisticRegression()).fit(training_data, train_target)
+    print('One vs rest accuracy: % .3f' % OVR.score(test_data, test_target))
 
 
 
-def dummy_classifier():
+def dummy_classifier(column):
     # not working yet :( sniff..
-    training_data, test_data, train_target, test_target = divide_data()
+    training_data, test_data, train_target, test_target = prepare_data(column)
     dummy = DummyClassifier('stratified')
 
     dummy.fit(training_data, train_target)
@@ -139,7 +126,11 @@ def dummy_classifier():
 
 
 if __name__ == "__main__":
-    logistic_regression('rrday')
+    logistic_regression('tday')
+    #oneVSRest('tday')
     #some_regression_thing('snow')
-
     #dummy_classifier()
+
+
+
+
